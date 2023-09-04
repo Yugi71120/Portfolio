c957e3c678d73984c63e51d1194d4e4292594379
Yugendhar <86682458+Yugi71120@users.noreply.github.com>
2023-09-04 09:01:54 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -76,7 +76,7 @@ const Banner = () => {
 							viewport={{ once: false, amount: 0.7 }}
 							className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
 						>
-                    <a href="/Yugendhar-CV.pdf" download className="btn btn-lg" style={{ display: 'inline-flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
+                    <a href="/Resume.pdf" download className="btn btn-lg" style={{ display: 'inline-flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
                     <FaArrowDown /> Download CV
                           </a>
 							<a href="#contact" className="text-gradient btn-link">
